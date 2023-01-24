df53ac2b1a10ccd9bc7291625397147470a49cdf
Sanky2024 <48628985+Sanky2024@users.noreply.github.com>
2023-01-24 14:54:12 +0530

    Create second.groovy

---
+++ vars/second.groovy
@@ -0,0 +1,16 @@
+def add(x,y)
+{
+  echo "Sum of ${x} and ${y} is $(x+y)"
+}
+def miniuis(x,y)
+{
+  echo "Minus of ${x} and ${y} is $(x-y)"
+}
+def multi(x,y)
+{
+  echo "Multi of ${x} and ${y} is $(x*y)"
+}
+def devi(x,y)
+{
+  echo "Div of ${x} and ${y} is $(x/y)"
+}
