412d7ddf7d358ad17e46fd017eaa9bd3122d7a85
Sanky2024 <48628985+Sanky2024@users.noreply.github.com>
2023-01-24 15:10:29 +0530

    Update second.groovy

--- vars/second.groovy
+++ vars/second.groovy
@@ -1,16 +1,16 @@
 def add(x,y)
 {
-  echo "Sum of "${x}" and "${y}" is "$(x+y)""
+  echo "Sum of ${x} and ${y} is $(x+y)"
 }
-// def minus(x,y)
-// {
-//   echo "Minus of ${x} and ${y} is $(x-y)"
-// }
-// def multi(x,y)
-// {
-//   echo "Multi of ${x} and ${y} is $(x*y)"
-// }
-// def divi(x,y)
-// {
-//   echo "Div of ${x} and ${y} is $(x/y)"
-// }
+def minus(x,y)
+{
+  echo "Minus of ${x} and ${y} is $(x-y)"
+}
+def multi(x,y)
+{
+  echo "Multi of ${x} and ${y} is $(x*y)"
+}
+def divi(x,y)
+{
+  echo "Div of ${x} and ${y} is $(x/y)"
+}
